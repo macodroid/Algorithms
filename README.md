6961d3d25736ae81b1ee676c19212fe578d4fe63
maco <macko.andrej1@gmail.com>
2021-10-04 16:26:04 +0200

    started with linkedList not happy about ressoult

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Algorithms
+
+## Data Structure
+- [ ] Linked List
+- [ ] DoubleLinked List
+- [ ] Queue
+- [ ] Stack
+- [ ] some tries
+- [ ] priority queue
---
+++ data_structure/linkedList.py
@@ -0,0 +1,23 @@
+# functions to implement insert_start, insert_end, print_ll, 
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, Node):
+        self.head_node = None
+
+    def insert_node_at_end(self, data):
+        while 
+
+ll = LinkedList()
+first_node = Node(3)
+second_node = Node(11)
+third_node = Node(38)
+
+ll.insert_node_at_end(first_node)
+ll.insert_node_at_end(first_node)
+ll.insert_node_at_end(first_node)
+        
